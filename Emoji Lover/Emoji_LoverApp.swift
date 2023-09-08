a79bb2ff2f371e35e3047505b8bbba95f2dbc508
Guinter Zaffalon <guinterzafa@gmail.com>
2023-09-08 15:30:52 -0300

    Initial Commit

---
+++ Emoji Lover/Emoji_LoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_LoverApp.swift
+//  Emoji Lover
+//
+//  Created by Guinter Zaffalon on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_LoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
